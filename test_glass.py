2b3fb3623fba9f16929e34ef8a3218ce03b3268c
Frédérik Paradis <fredy_14@live.fr>
2015-04-15 21:57:19 -0400

    adding python test code for dataset

---
+++ test_glass.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import numpy as np
+from sklearn import preprocessing
+from loader import load_glass
+from test import Tester
+
+nbTrain = 35
+#nbTest = 2000
+
+
+print "Beginning of loading."
+X, Y, text_features, class_dict = load_glass();
+normal_class = 1
+
+nbTest = len(X) - nbTrain
+
+X = preprocessing.normalize(X)
+anomaly = [x for x, y in zip(X, Y) if y != normal_class]
+normal = [x for x, y in zip(X, Y) if y == normal_class]
+print "End of loading."
+
+print len(normal)
+print len(anomaly)
+
+test = Tester(X, Y, normal, anomaly, nbTrain, nbTest)
+test.hyperspherical_predictor()
+test.one_class_svm()
+"""
+test.multiclass_hyperspherical_predictor()
+test.multiclass_one_class_svm()
+test.svc()
+"""
