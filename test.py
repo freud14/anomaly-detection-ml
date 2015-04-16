85688b989d7c33288b709c3288a71e9f416df66b
Frédérik Paradis <fredy_14@live.fr>
2015-04-15 20:56:39 -0400

    refactoring

--- test.py
+++ test.py
@@ -1,88 +1,76 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 import numpy as np
-from sklearn import datasets, preprocessing, svm, multiclass
+from sklearn import svm, multiclass
 from hyperspherical import HypersphericalPredictor
-from loader import *
 from util import *
 
-nbTrain = 1000
-nbTest = 2000
+class Tester:
+    def __init__(self, X, Y, normal, anomaly, nbTrain, nbTest):
+        self.X = X
+        self.Y = Y
+        self.normal = normal
+        self.anomaly = anomaly
+        self.nbTrain = nbTrain
+        self.nbTest = nbTest
 
+    def hyperspherical_predictor(self, gamma_hypersphere=0.5):
+        train_normal = self.normal[:self.nbTrain]
+        test_normal = self.normal[self.nbTrain:self.nbTrain + self.nbTest]
+        test_anormal = self.anomaly[:self.nbTest]
 
-print "Beginning of loading."
-#X, Y, text_features, class_dict = load_arrhythmia();
-#X, Y, text_features, class_dict = load_breastcancerwisconsin();
-#X, Y, text_features, class_dict = load_ecoli();
-#X, Y, text_features, class_dict = load_glass();
-#X, Y, text_features, class_dict = load_iris();
-#X, Y, text_features, class_dict = load_liver();
+        print "Beginning of testing of the hyperspherical predictor..."
+        # Test du prédicteur qui fonctionne avec une hypersphère
+        hypersphere = HypersphericalPredictor(kernel='rbf', gamma=gamma_hypersphere)
+        hypersphere.fit(train_normal)
 
-X, Y, text_features, class_dict = load_packets();
+        test_normal_predict = hypersphere.predict(test_normal)
+        print risk(test_normal_predict, np.full(len(test_normal_predict), 1))
 
-#nbTest = len(X) - nbTrain
+        test_anormal_predict = hypersphere.predict(test_anormal)
+        print risk(test_anormal_predict, np.full(len(test_anormal_predict), -1))
+        print "End of testing of the hyperspherical predictor."
 
-X = preprocessing.normalize(X)
-anomaly = [x for x, y in zip(X, Y) if y != class_dict['normal']]
-normal = [x for x, y in zip(X, Y) if y == class_dict['normal']]
+    def one_class_svm(self, gamma_one_class=0.5):
+        train_normal = self.normal[:self.nbTrain]
+        test_normal = self.normal[self.nbTrain:self.nbTrain + self.nbTest]
+        test_anormal = self.anomaly[:self.nbTest]
 
-train = normal[:nbTrain]
-test_normal = normal[nbTrain:nbTrain + nbTest]
-test_anormal = anomaly[:nbTest]
+        print "Beginning of testing of the one-class SVM..."
+        # Test du one-class SVM
+        one_class_svm = svm.OneClassSVM(nu=0.01, kernel='rbf', gamma=gamma_one_class)
+        one_class_svm.fit(train_normal)
 
-print "End of loading."
+        test_normal_predict = one_class_svm.predict(test_normal)
+        print risk(test_normal_predict, np.full(len(test_normal_predict), 1))
 
+        test_anormal_predict = one_class_svm.predict(test_anormal)
+        print risk(test_anormal_predict, np.full(len(test_anormal_predict), -1))
+        print "End of testing of the one-class SVM."
 
+    def multiclass_hyperspherical_predictor(self, gamma_multiclass_hypersphere=0.5):
+        print "Beginning of testing of the multiclass hyperspherical predictor..."
+        # Test du prédicteur d'hypersphère en le transformant en prédicteur multi-classe.
+        multiclass_hypersphere = multiclass.OneVsRestClassifier(HypersphericalPredictor(kernel='rbf', gamma=gamma_multiclass_hypersphere))
+        multiclass_hypersphere.fit(self.X[:self.nbTrain], self.Y[:self.nbTrain])
 
-print "Beginning of testing of the hyperspherical predictor..."
-# Test du prédicteur qui fonctionne avec une hypersphère
-hypersphere = HypersphericalPredictor(kernel='rbf', gamma=0.5)
-hypersphere.fit(train)
+        test_predict = multiclass_hypersphere.predict(self.X[self.nbTrain:self.nbTrain + self.nbTest])
+        print risk(test_predict, self.Y[self.nbTrain:self.nbTrain + self.nbTest])
+        print "End of testing of the multiclass hyperspherical predictor..."
 
-test_normal_predict = hypersphere.predict(test_normal)
-print risk(test_normal_predict, np.full(len(test_normal_predict), 1))
+    def multiclass_one_class_svm(self, gamma_multiclass_oneclass=0.5):
+        print "Beginning of testing of the multiclass one-class SVM predictor..."
+        # Test du one-class SVM en le transformant en prédicteur multi-classe.
+        multiclass_oneclass = multiclass.OneVsRestClassifier(svm.OneClassSVM(nu=0.01, kernel='rbf', gamma=gamma_multiclass_oneclass))
+        multiclass_oneclass.fit(self.X[:self.nbTrain], self.Y[:self.nbTrain])
 
-test_anormal_predict = hypersphere.predict(test_anormal)
-print risk(test_anormal_predict, np.full(len(test_anormal_predict), -1))
-print "End of testing of the hyperspherical predictor."
+        test_predict = multiclass_oneclass.predict(self.X[self.nbTrain:self.nbTrain + self.nbTest])
+        print risk(test_predict, self.Y[self.nbTrain:self.nbTrain + self.nbTest])
+        print "End of testing of the multiclass hyperspherical predictor..."
 
-"""
-print "Beginning of testing of the one-class SVM..."
-# Test du one-class SVM
-one_class_svm = svm.OneClassSVM(nu=0.01, kernel='rbf', gamma=0.5)
-one_class_svm.fit(train)
+    def svc(self, gamma_svc=0.5):
+        svc = svm.SVC(kernel='rbf', gamma=gamma_svc)
+        svc.fit(self.X[:self.nbTrain], self.Y[:self.nbTrain])
 
-test_normal_predict = one_class_svm.predict(test_normal)
-print risk(test_normal_predict, np.full(len(test_normal_predict), 1))
-
-test_anormal_predict = one_class_svm.predict(test_anormal)
-print risk(test_anormal_predict, np.full(len(test_anormal_predict), -1))
-print "End of testing of the one-class SVM."
-"""
-"""
-print "Beginning of testing of the multiclass hyperspherical predictor..."
-# Test du prédicteur d'hypersphère en le transformant en prédicteur multi-classe.
-multiclass_hypersphere = multiclass.OneVsRestClassifier(HypersphericalPredictor(kernel='rbf', gamma=0.5))
-multiclass_hypersphere.fit(X[:nbTrain], Y[:nbTrain])
-
-test_predict = multiclass_hypersphere.predict(X[nbTrain:nbTrain + nbTest])
-print risk(test_predict, Y[nbTrain:nbTrain + nbTest])
-print "End of testing of the multiclass hyperspherical predictor..."
-"""
-"""
-print "Beginning of testing of the multiclass one-class SVM predictor..."
-# Test du one-class SVM en le transformant en prédicteur multi-classe.
-multiclass_hypersphere = multiclass.OneVsRestClassifier(svm.OneClassSVM(nu=0.01, kernel='rbf', gamma=0.5))
-multiclass_hypersphere.fit(X[:nbTrain], Y[:nbTrain])
-
-test_predict = multiclass_hypersphere.predict(X[nbTrain:nbTrain + nbTest])
-print risk(test_predict, Y[nbTrain:nbTrain + nbTest])
-print "End of testing of the multiclass hyperspherical predictor..."
-"""
-"""
-svc = svm.SVC(kernel='rbf', gamma=0.5)
-svc.fit(X[:nbTrain], Y[:nbTrain])
-
-test_predict = svc.predict(X[nbTrain:nbTrain + nbTest])
-print risk(test_predict, Y[nbTrain:nbTrain + nbTest])
-"""
+        test_predict = svc.predict(self.X[self.nbTrain:self.nbTrain + self.nbTest])
+        print risk(test_predict, self.Y[self.nbTrain:self.nbTrain + self.nbTest])
---
+++ test_packets.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+import numpy as np
+from sklearn import preprocessing
+from loader import load_packets
+from test import Tester
+
+nbTrain = 1000
+nbTest = 2000
+
+
+print "Beginning of loading."
+#X, Y, text_features, class_dict = load_arrhythmia();
+#X, Y, text_features, class_dict = load_breastcancerwisconsin();
+#X, Y, text_features, class_dict = load_ecoli();
+#X, Y, text_features, class_dict = load_glass();
+#X, Y, text_features, class_dict = load_iris();
+#X, Y, text_features, class_dict = load_liver();
+
+X, Y, text_features, class_dict = load_packets();
+
+#nbTest = len(X) - nbTrain
+
+X = preprocessing.normalize(X)
+anomaly = [x for x, y in zip(X, Y) if y != class_dict['normal']]
+normal = [x for x, y in zip(X, Y) if y == class_dict['normal']]
+print "End of loading."
+
+test = Tester(X, Y, normal, anomaly, nbTrain, nbTest)
+test.hyperspherical_predictor()
+test.one_class_svm()
+test.multiclass_hyperspherical_predictor()
+test.multiclass_one_class_svm()
+test.svc()
